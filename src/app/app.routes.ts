0171d748163bb50b0a735fa1a90486a2c2db10f9
PabloLCmrgo <48867244+PabloLCmrgo@users.noreply.github.com>
2026-07-01 22:06:45 -0600

    routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MovieListComponent } from './components/movies/movie-list/movie-list.component';
+import { MovieFormComponent } from './components/movies/movie-form/movie-form.component';
+
+import { DirectorListComponent } from './components/directors/director-list/director-list.component';
+import { DirectorFormComponent } from './components/directors/director-form/director-form.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'movies',
+    component: MovieListComponent,
+  },
+
+  {
+    path: 'movies/new',
+    component: MovieFormComponent,
+  },
+
+  {
+    path: 'movies/edit/:id',
+    component: MovieFormComponent,
+  },
+
+  {
+    path: 'directors',
+    component: DirectorListComponent,
+  },
+
+  {
+    path: 'directors/new',
+    component: DirectorFormComponent,
+  },
+
+  {
+    path: 'directors/edit/:id',
+    component: DirectorFormComponent,
+  },
+
+  {
+    path: '**',
+    redirectTo: 'movies',
+  },
+];
