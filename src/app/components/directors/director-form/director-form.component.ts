80c6e8ee8a6df6a998022668bb5a3772981910ee
PabloLCmrgo <48867244+PabloLCmrgo@users.noreply.github.com>
2026-07-01 21:56:30 -0600

    first commit

---
+++ src/app/components/directors/director-form/director-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-director-form',
+  imports: [],
+  templateUrl: './director-form.component.html',
+  styleUrl: './director-form.component.css'
+})
+export class DirectorFormComponent {
+
+}
---
+++ src/app/components/directors/director-list/director-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-director-list',
+  imports: [],
+  templateUrl: './director-list.component.html',
+  styleUrl: './director-list.component.css'
+})
+export class DirectorListComponent {
+
+}
